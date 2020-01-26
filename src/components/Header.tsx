8baf394127d2778b90a7688c625f91f55dd153ec
Michał Strzelczyk <mich.strzelczyk@gmail.com>
2020-01-26 16:25:49 +0100

    Add Github link with icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles';
+import GitHub from '@material-ui/icons/GitHub'
+import Link from '@material-ui/core/Link'
+
+const GITHUB_URL = "https://github.com/panmichal/midi-web";
 
 const useStyles = makeStyles({
     root: {
@@ -11,14 +15,30 @@ const useStyles = makeStyles({
         fontFamily: 'Futura',
         fontSize: '30',
         verticalAlign: 'middle',
-        display: 'flex'
+        display: 'flex',
+        justifyContent: 'space-between'
     },
     headerText: {
         alignSelf: 'center'
+    },
+    headerRight: {
+        a: {
+            color: "white"
+        },
+        alignSelf: 'center'
+    },
+    headerRightLink: {
+        color: "white",
+        '&:hover': {
+            color: "grey"
+        }
     }
 });
 
 export default function Header() {
     const classes = useStyles()
-    return <div className={classes.root}><div className={classes.headerText}>🎹 MIDI TOOLS</div></div>
+    return <div className={classes.root}>
+        <div className={classes.headerText}>🎹 MIDI TOOLS</div>
+        <div className={classes.headerRight}><Link target="black" className={classes.headerRightLink} href={GITHUB_URL}><GitHub /></Link></div>
+    </div>
 }
