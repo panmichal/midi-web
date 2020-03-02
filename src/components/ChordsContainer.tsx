a9c5a172d767c3018d05b0e8a3949408ac56bdec
Michał Strzelczyk <mich.strzelczyk@gmail.com>
2020-03-02 19:21:07 +0100

    Add Tonal and use chord recognition

--- src/components/ChordsContainer.tsx
+++ src/components/ChordsContainer.tsx
@@ -1,9 +1,14 @@
 import { connect } from 'react-redux';
 import { RootState } from '~/store/index';
+import { PlayedNote } from '~/store/events/types';
 import Chords from '~/components/Chords';
+import { chordType } from '@tonaljs/chord-dictionary';
+import { pcset } from '@tonaljs/pcset';
 
 export const mapStateToProps: (state: RootState) => { chords: string[] } = state => {
-    return { chords: ['A#', 'C'] };
+    const notes = state.event.notesPlaying.map(note => note.noteName);
+    console.log(chordType(pcset(notes).chroma));
+    return { chords: [chordType(pcset(notes).chroma).name] };
 };
 
 const ChordsContainer = connect(mapStateToProps)(Chords);
